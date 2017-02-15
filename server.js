f8ee25c9b6d5648966bd74aee3288bc03116d671
ezat123 <ezat.bscs@gmail.com>
2017-02-15 19:55:29 +0500

    app.user false

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express()
 var port = process.env.PORT || 5000
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({	extended: true })); // support encoded bodies
+app.use(bodyParser.urlencoded({	extended: false })); // support encoded bodies
 
 var SchemaOb = new mongoose.Schema({
     userName: String,
