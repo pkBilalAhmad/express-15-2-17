23b7b721236712dcf518207d6481abf15a728a24
ezat123 <ezat.bscs@gmail.com>
2017-02-15 11:51:20 +0500

    mongoose and post api added

--- server.js
+++ server.js
@@ -5,27 +5,44 @@ var app = express()
 
 var SchemaOb = new mongoose.Schema({
     userName: String,
-    firstName : String,
-    lastName : String
+    firstName: String,
+    lastName: String
 });
 
 
 var SchemaObModel = mongoose.model('bilalSchema', SchemaOb);
 
 
+app.post('/post', function (req, res) {
+    var schemaObModelOb = new SchemaObModel({
+        userName: req.body.userName,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName
+    })
+    schemaObModelOb.save(function(err,data){
+        if(!err){
+            res.send("schemaObModelOb-Object is saved")
+            console.log("Object is saved")
+        }else{
+            console.log("Object saving failed");
+            req.send("Object saving failed")
+        }
+    })
+})
+
 
 
 
 app.get('/', function (req, res) {
-  res.send('Hello Karachi 15-2-17')
+    res.send('Hello Karachi 15-2-17')
 })
 
 app.get('/home', function (req, res) {
-  res.send('home')
+    res.send('home')
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+    console.log('Example app listening on port 3000!')
 })
 
 
